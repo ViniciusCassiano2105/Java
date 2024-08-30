76499b9b189e4f36a06efdd3ca917c76fab02447
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-08-30 11:17:41 -0300

    metodos

---
+++ meus_projetos/src/br/com/exercicio_1_como_usar_variaveis/estruturas_basicas.java
@@ -0,0 +1,21 @@
+package br.com.exercicio_1_como_usar_variaveis;
+
+public class estruturas_basicas {
+
+	public static void main(String[] args) {
+	
+		/* Sintaxe de declaração das variaveis no Java
+		 * <tipo > identificador;
+		 * <tipo> identificador = valor;
+		 * 
+		 * Variáveis tem escopos: bloco de comandos no qual 
+		 * a variável foi declarada.
+		 * *
+		 */
+		
+		String saudacao = "Olá, Vinicius";
+		System.out.println(saudacao);
+
+	}
+
+}
