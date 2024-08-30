4508cb83f1fe44e2870c439211d2f21a5b9fb3cc
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-08-30 17:23:36 -0300

    Adição exercício tipos_primitivos.

--- meus_projetos/src/br/com/introducao_java/tipos_primitivos.java
+++ meus_projetos/src/br/com/introducao_java/tipos_primitivos.java
@@ -4,7 +4,37 @@ public class tipos_primitivos {
 
 	public static void main(String[] args) {
 		// tipos numéricos inteiros - byte, short, int, long
+//			int numero1 = 1;
+//			int numero2 = 2;
+//			int soma = numero1 + numero2;
+//			System.out.println(soma);
+			
+//			// Tipos decimais - float, double
+//			double numero1 = 1.5;
+//			double numero2 = 1.5;
+//			double soma = numero1 + numero2;
+//			System.out.println(soma);
+			
+			//Char e boolean
+//			char operador = '+';
+//			double resultado = soma( 2.5, 7.5);
+//			System.out.println(operador + ": " + resultado);
+		
+		//boolean
+		char operador = '+';
+		
+		double numero1 = 1.5;
+		double numero2 = 2.5;
+		
+		double resultado = soma(numero1, numero2);
+		System.out.println(operador + ": " + resultado);
+		
+		boolean ehIgual = numero1 < numero2;
+		System.out.println(ehIgual);
+		
 			
 	}
-
+		static double soma(double numero1, double numero2) {
+			return numero1 + numero2;
+		}
 }
