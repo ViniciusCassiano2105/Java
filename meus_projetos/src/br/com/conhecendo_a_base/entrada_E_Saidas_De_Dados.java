9ca86921ee0160131988afa74e1047d7b0c5fcdd
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-02 12:00:50 -0300

    Entrada e Saída de dados.

---
+++ meus_projetos/src/br/com/conhecendo_a_base/entrada_E_Saidas_De_Dados.java
@@ -0,0 +1,27 @@
+package br.com.conhecendo_a_base;
+
+import java.util.Scanner;
+
+public class entrada_E_Saidas_De_Dados {
+	/*
+	 * Entrada e Saída (I/O) de dados no java.
+	 * 
+	 * Entrada de dados: Ler dados digitados pelo usuário.
+	 * Saída de dados: Escrever dados para o usuário.
+	 */
+
+	public static void main(String[] args) {
+//		// Leitura de dados de entrada
+//		System.out.println("Digite um numero:");
+//		Scanner scanner = new Scanner (System.in);
+//		int valorDigitado = scanner.nextInt();
+//		System.out.println(valorDigitado);
+//		scanner.close();
+		
+		// Saída de dados
+		float qualquerNumero = 3.0f;
+		System.out.printf("O número digitado foi %f" , qualquerNumero);
+
+	}
+
+}
