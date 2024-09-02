a0026d0f92d4d460e3ae7bb3657a818deeb33951
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-02 10:14:54 -0300

    conhecendo a base/Variáveis.

---
+++ meus_projetos/src/br/com/conhecendo_a_base/variaveis.java
@@ -0,0 +1,16 @@
+package br.com.conhecendo_a_base;
+
+public class variaveis {
+
+	public static void main(String[] args) {
+		int dia = 29;
+		int mes = 04;
+		int ano = 2024;
+		
+		System.out.println(dia);
+		System.out.println(mes);
+		System.out.println(ano);
+
+	}
+
+}
