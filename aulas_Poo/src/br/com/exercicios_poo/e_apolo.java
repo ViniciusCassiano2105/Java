d7fdf11ce73ea29f24b034296f701a7f4cc88e2a
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-03 19:04:19 -0300

    Exercicio apolo.

---
+++ aulas_Poo/src/br/com/exercicios_poo/e_apolo.java
@@ -0,0 +1,13 @@
+package br.com.exercicios_poo;
+
+public class e_apolo {
+
+	public static void main(String[] args) {
+		int soma;
+		
+		System.out.println("a. 2 + 3 - 5 * 8 - 4 + 354 - 521 + 7 * 66 é igual a:");
+		soma = 2 + 3 - (5 * 8) - 4 + 354 - 521 + (7 * 66);
+		System.out.printf("%d",soma);
+	}
+
+}
