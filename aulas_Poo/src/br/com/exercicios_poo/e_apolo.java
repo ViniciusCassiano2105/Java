d9b6919921a9214685142bca88d62f834ef9ff32
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-03 19:17:13 -0300

    Correção exercicio Apolo

--- aulas_Poo/src/br/com/exercicios_poo/e_apolo.java
+++ aulas_Poo/src/br/com/exercicios_poo/e_apolo.java
@@ -4,10 +4,14 @@ public class e_apolo {
 
 	public static void main(String[] args) {
 		int soma;
+		int segundaSoma;
 		
 		System.out.println("a. 2 + 3 - 5 * 8 - 4 + 354 - 521 + 7 * 66 é igual a:");
 		soma = 2 + 3 - (5 * 8) - 4 + 354 - 521 + (7 * 66);
-		System.out.printf("%d",soma);
+		System.out.printf("%d\n",soma);
+		System.out.println("b. 2 + 7 * (14 - 21) + 28 * 3 * 42 + 740 - (156 + 4 + 40) * 9 é igual a:");
+		segundaSoma = 2 + 7 * (14 - 21) + 28 * 3 * 42 + 740 - (156 + 4 + 40) * 9;
+		System.out.printf("%d",segundaSoma);
 	}
 
 }
