dac52b552e5acad1a3b0b68e32d05954d1e01927
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-04 09:16:30 -0300

    Exercício Double.

--- aulas_Poo/src/br/com/exercicios_poo/DoubleBalancoTri.java
+++ aulas_Poo/src/br/com/exercicios_poo/DoubleBalancoTri.java
@@ -3,7 +3,20 @@ package br.com.exercicios_poo;
 public class DoubleBalancoTri {
 
 	public static void main(String[] args) {
+			System.out.println
+			("Em nossa empresa, há tabelas com o quanto foi gasto em cada mês. Para fechar o balanço do\r\n"
+			+ "primeiro trimestre, precisamos somar o gasto total. Sabendo que, em Janeiro, foram gastos R$15000,\r\n"
+			+ "em Fevereiro, R$ 23000, e em Março, R$ 17000, faça um programa que calcule e imprima\r\n"
+			+ "o gasto total no trimestre.");
 		
+			Double gastosJaneiro = 15000.0;
+			Double gastosFevereiro = 23000.0;
+			Double gastosMarco = 17000.0;
+			Double gastosTrimestre;
+			
+			gastosTrimestre= gastosJaneiro + gastosFevereiro + gastosMarco;
+			
+			System.out.println("O gasto trimestral da empresa é: R$" + gastosTrimestre);
 	}
 	
 }
