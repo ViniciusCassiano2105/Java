879ebb8ca81b1432c300b78122d0807b53ba8453
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-04 11:08:38 -0300

    Exercício universo.

---
+++ aulas_Poo/src/br/com/exercicios_poo/a_2_Universo.java
@@ -0,0 +1,13 @@
+package br.com.exercicios_poo;
+
+public class a_2_Universo {
+
+	public static void main(String[] args) {
+		int UNIVERSO = 42;
+		// Exibe a pergunta e o número do universo
+		System.out.println("“Qual o significado da vida, do universo e tudo mais?”");
+		System.out.println("O número do universo é: " + UNIVERSO);
+
+	}
+
+}
