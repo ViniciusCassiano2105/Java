9e7dec50865932be6f3f10c1f745c00254125d2b
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-04 11:49:30 -0300

    Tabuada

--- meus_projetos/src/br/com/conhecendo_a_base/exercicio_3.java
+++ meus_projetos/src/br/com/conhecendo_a_base/exercicio_3.java
@@ -13,11 +13,20 @@ public class exercicio_3 {
 	public static void main(String[] args) {
 		
 		int numeroUsuario;
-		int numeroTabuada;
 		
-		System.out.println("Escreva um número de 1 a 10 para saber o resultado.");
+		System.out.println("Escreva um número de 1 a 10 para saber o resultado da multiplicação.");
 		Scanner scanner = new Scanner(System.in);
 		numeroUsuario = scanner.nextInt();
+		System.out.println(numeroUsuario + " x 1 = " + (1 * numeroUsuario));
+		System.out.println(numeroUsuario + " x 2 = " + (2 * numeroUsuario));
+		System.out.println(numeroUsuario + " x 3 = " + (3 * numeroUsuario));
+		System.out.println(numeroUsuario + " x 4 = " + (4 * numeroUsuario));
+		System.out.println(numeroUsuario + " x 5 = " + (5 * numeroUsuario));
+		System.out.println(numeroUsuario + " x 6 = " + (6 * numeroUsuario));
+		System.out.println(numeroUsuario + " x 7 = " + (7 * numeroUsuario));
+		System.out.println(numeroUsuario + " x 8 = " + (8 * numeroUsuario));
+		System.out.println(numeroUsuario + " x 9 = " + (9 * numeroUsuario));
+		System.out.println(numeroUsuario + " x 10 = " + (10 * numeroUsuario));
 		scanner.close();
 		
 		
