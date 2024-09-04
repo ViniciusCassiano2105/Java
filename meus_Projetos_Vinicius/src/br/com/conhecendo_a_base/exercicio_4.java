b58f94b6d3e7c9d71cb658ec4cac98fce9b4357d
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-04 12:46:14 -0300

    RelacionamentosDeOrdem

---
+++ meus_Projetos_Vinicius/src/br/com/conhecendo_a_base/exercicio_4.java
@@ -0,0 +1,15 @@
+/*
+ * Escreva um programa que informe os relacionamentos de ordem existentes entre dois números: 
+ * Igual, não igual, maior, menor, maior ou igual, menor ou igual.
+ * 
+ * 1. Ler 2 números
+ * 2. Imprimir relacionamentos de ordem
+ * 
+ * Ex: Dados os números 2 e 3: 
+ *  2 = 3 ? false, 2 != 3 ? true, ... , 2 <= 3 ? true
+ */
+public class RelacionamentosDeOrdem {
+  public static void main(String[] args) {
+    // Crie o seu programa aqui...
+  }
+}
