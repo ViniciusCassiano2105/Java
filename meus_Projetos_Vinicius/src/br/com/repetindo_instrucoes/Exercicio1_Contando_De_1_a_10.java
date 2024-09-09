b8bc0658da8968cd265c5132666d63ee70524d95
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-09 11:19:19 -0300

    Alteração

--- meus_Projetos_Vinicius/src/br/com/repetindo_instrucoes/Exercicio1_Contando_De_1_a_10.java
+++ meus_Projetos_Vinicius/src/br/com/repetindo_instrucoes/Exercicio1_Contando_De_1_a_10.java
@@ -2,7 +2,7 @@ package br.com.repetindo_instrucoes;
 //Escreva um programa Java que imprime números de 1 à 10 
 //utilizando a estrutura de repetição while. 
 //O programa deve imprimir cada número em uma linha diferente.
-public class Exercicio1_Contando_de_1_a_10 {
+public class Exercicio1_Contando_De_1_a_10 {
 
 	public static void main(String[] args) {
 		int i;
@@ -11,6 +11,7 @@ public class Exercicio1_Contando_de_1_a_10 {
 		for (i = 1; i <= 10; i++) {
 			if(i>=1) {
 			System.out.println(i);
+			
 			}
 		
 			
