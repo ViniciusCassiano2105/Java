54c150dcb0a5ca1667f7ab01aa43a89541816847
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-09 11:19:04 -0300

    Correção

--- meus_Projetos_Vinicius/src/br/com/repetindo_instrucoes/Exercicio1_Soma_dos_numero_de_1_a_N.java
+++ meus_Projetos_Vinicius/src/br/com/repetindo_instrucoes/Exercicio1_Soma_dos_numero_de_1_a_N.java
@@ -1,5 +1,7 @@
 package br.com.repetindo_instrucoes;
-
+//Escreva um programa Java que imprime números de 1 à 10 
+//utilizando a estrutura de repetição while. 
+//O programa deve imprimir cada número em uma linha diferente.
 public class Exercicio1_Contando_de_1_a_10 {
 
 	public static void main(String[] args) {
