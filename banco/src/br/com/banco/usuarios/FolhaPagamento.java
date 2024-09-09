8ff047b5be485eb772b242ce79aded56dbb55d74
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-09 16:15:44 -0300

    Exercicios

---
+++ banco/src/br/com/banco/usuarios/FolhaPagamento.java
@@ -0,0 +1,19 @@
+package br.com.banco.usuarios;
+
+public class FolhaPagamento {
+
+    // metodo Polichatismo
+    public void calcularFolha(Funcionario funcionario) {
+        double salario = funcionario.calcularSalario();
+        System.out.println("O salário de " + funcionario.getNome() + " é: " + salario);
+    }
+
+    public static void main(String[] args) {
+        Funcionario func1 = new Funcionario("Lucas", "12345678900", 2000.0, 1234, "LucasObon");
+        Gerente gerente1 = new Gerente("Leticia", "98765432100", 10000.0, 5678, "Leticia123");
+
+        FolhaPagamento folha = new FolhaPagamento();
+        folha.calcularFolha(func1);    
+        folha.calcularFolha(gerente1); 
+    }
+}
