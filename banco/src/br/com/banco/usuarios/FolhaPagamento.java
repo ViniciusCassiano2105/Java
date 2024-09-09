03da7d113ec32982dece853ff79828128210d09d
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-09 16:23:19 -0300

    Folha de pagamento

--- banco/src/br/com/banco/usuarios/FolhaPagamento.java
+++ banco/src/br/com/banco/usuarios/FolhaPagamento.java
@@ -2,15 +2,15 @@ package br.com.banco.usuarios;
 
 public class FolhaPagamento {
 
-    // metodo Polichatismo
+    // metodo Polimorfismo
     public void calcularFolha(Funcionario funcionario) {
         double salario = funcionario.calcularSalario();
         System.out.println("O salário de " + funcionario.getNome() + " é: " + salario);
     }
 
     public static void main(String[] args) {
-        Funcionario func1 = new Funcionario("Lucas", "12345678900", 2000.0, 1234, "LucasObon");
-        Gerente gerente1 = new Gerente("Leticia", "98765432100", 10000.0, 5678, "Leticia123");
+        Funcionario func1 = new Funcionario("Lucas", "12345678900", 2000.0, 1234, "Lucas");
+        Gerente gerente1 = new Gerente("Leticia", "98765432100", 10000.0, 5678, "Leticia");
 
         FolhaPagamento folha = new FolhaPagamento();
         folha.calcularFolha(func1);    
