ef98f0ec0d0a757859803921a431453ae2ad1e4f
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-09 09:52:55 -0300

    Exercício 1 - Contando de 1 à 10

---
+++ meus_Projetos_Vinicius/src/br/com/repetindo_instrucoes/Exercicio1_Contando_de_1_a_10.java
@@ -0,0 +1,19 @@
+package br.com.repetindo_instrucoes;
+
+public class Exercicio1_Contando_de_1_a_10 {
+
+	public static void main(String[] args) {
+		int i;
+		
+		
+		for (i = 1; i <= 10; i++) {
+			if(i>=1) {
+			System.out.println(i);
+			}
+		
+			
+		}
+
+	}
+
+}
