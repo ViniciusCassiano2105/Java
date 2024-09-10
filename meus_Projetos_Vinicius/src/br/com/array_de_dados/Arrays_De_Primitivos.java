bad092c78632ebf68c662ab2b11293f90873467e
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-10 08:22:04 -0300

    Arrays.toString

---
+++ meus_Projetos_Vinicius/src/br/com/array_de_dados/Arrays_De_Primitivos.java
@@ -0,0 +1,14 @@
+package br.com.array_de_dados;
+
+import java.util.Arrays;
+
+public class Arrays_De_Primitivos {
+
+	public static void main(String[] args) {
+		int[] idades = {5, 10, 18, 30, 34};
+		
+		System.out.println(Arrays.toString(idades));
+
+	}
+
+}
