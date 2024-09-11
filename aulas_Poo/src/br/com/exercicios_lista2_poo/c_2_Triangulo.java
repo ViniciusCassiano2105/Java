471475776a8f38294e24a6f7b745f94260787788
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-11 13:54:31 -0300

    Correção exercicio triangulo

--- aulas_Poo/src/br/com/exercicios_lista2_poo/c_2_Triangulo.java
+++ aulas_Poo/src/br/com/exercicios_lista2_poo/c_2_Triangulo.java
@@ -12,14 +12,13 @@ public class c_2_Triangulo {
 		logger.warning("Insira os valores dos três ângulos de um triângulo: ");
 
 		logger.info("Primeiro ângulo:");
-		
+
 		// Criando um scanner para ler a entrada do usuário
 		Scanner scanner = new Scanner(System.in);
 
-			
 		// Lendo o valor do primeiro ângulo
 		int angulo1 = scanner.nextInt();
-		
+
 		// Lendo o valor do segundo ângulo
 		logger.info("Segundo ângulo:");
 		int angulo2 = scanner.nextInt();
@@ -27,20 +26,20 @@ public class c_2_Triangulo {
 		// Lendo o valor do terceiro ângulo
 		logger.info("Terceiro ângulo:");
 		int angulo3 = scanner.nextInt();
-		
-		//	Verifica se a soma dos ângulos é 180
+
+		// Verifica se a soma dos ângulos é 180
 		if (angulo1 + angulo2 + angulo3 != 180) {
 			logger.info("Os ângulos fornecidos não formam um triângulo válido.");
-			
-		//	Verifica se o triângulo é retângulo
+
+			// Verifica se o triângulo é retângulo
 		} else if (angulo1 == 90 || angulo2 == 90 || angulo3 == 90) {
 			logger.info("Retãngulo");
-			
-		//	Verificando se o triângulo é obtusângulo	
+
+			// Verificando se o triângulo é obtusângulo
 		} else if (angulo1 > 90 || angulo2 > 90 || angulo3 > 90) {
 			logger.info("Obtusângulo");
-			
-		// Se todos os ângulos forem menores que 90, o triângulo é acutângulo	
+
+			// Se todos os ângulos forem menores que 90, o triângulo é acutângulo
 		} else {
 			logger.info("Acutângulo");
 		}
