736b935289fbdf4027d6e80a01d8e770b9328010
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-11 13:52:40 -0300

    Exercício lista 2 - Triângulo

--- aulas_Poo/src/br/com/exercicios_lista2_poo/c_2_Triangulo.java
+++ aulas_Poo/src/br/com/exercicios_lista2_poo/c_2_Triangulo.java
@@ -1,29 +1,51 @@
-package br.com.exercicios_poo;
+package br.com.exercicios_lista2_poo;
 
 import java.util.Scanner;
+import java.util.logging.Logger;
+
+import br.com.aula_poo.utils.Util;
 
 public class c_2_Triangulo {
+	private static Logger logger = Logger.getLogger(Util.class.getName());
 
 	public static void main(String[] args) {
-		System.out.println("Insira os valores dos três ângulos de um triângulo: ");
-		System.out.println("Primeiro ângulo:");
-		Scanner scanner = new Scanner(System.in);
-		int angulo1 = scanner.nextInt();
-		System.out.println("Segundo ângulo:");
-		int angulo2 = scanner.nextInt();
-		System.out.println("Terceiro ângulo:");
-		int angulo3 = scanner.nextInt();
-		scanner.close();
-		if(angulo1 + angulo2 + angulo3 != 180) {
-			System.out.println("Os ângulos fornecidos não formam um triângulo válido.");
-		}else if(angulo1 == 90 || angulo2 == 90 || angulo3 ==90 ){
-			System.out.println("Retãngulo");
-		}else if(angulo1 > 90 || angulo2 > 90 || angulo3 > 90 ){
-			System.out.println("Obtusângulo");
-		}else{
-			System.out.println("Acutângulo");
-		}
+		logger.warning("Insira os valores dos três ângulos de um triângulo: ");
+
+		logger.info("Primeiro ângulo:");
 		
+		// Criando um scanner para ler a entrada do usuário
+		Scanner scanner = new Scanner(System.in);
+
+			
+		// Lendo o valor do primeiro ângulo
+		int angulo1 = scanner.nextInt();
+		
+		// Lendo o valor do segundo ângulo
+		logger.info("Segundo ângulo:");
+		int angulo2 = scanner.nextInt();
+
+		// Lendo o valor do terceiro ângulo
+		logger.info("Terceiro ângulo:");
+		int angulo3 = scanner.nextInt();
+		
+		//	Verifica se a soma dos ângulos é 180
+		if (angulo1 + angulo2 + angulo3 != 180) {
+			logger.info("Os ângulos fornecidos não formam um triângulo válido.");
+			
+		//	Verifica se o triângulo é retângulo
+		} else if (angulo1 == 90 || angulo2 == 90 || angulo3 == 90) {
+			logger.info("Retãngulo");
+			
+		//	Verificando se o triângulo é obtusângulo	
+		} else if (angulo1 > 90 || angulo2 > 90 || angulo3 > 90) {
+			logger.info("Obtusângulo");
+			
+		// Se todos os ângulos forem menores que 90, o triângulo é acutângulo	
+		} else {
+			logger.info("Acutângulo");
+		}
+		// Fecha o scanner
+		scanner.close();
 	}
 
 }
