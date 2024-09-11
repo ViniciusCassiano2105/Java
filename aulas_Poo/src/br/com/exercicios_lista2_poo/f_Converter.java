c41f34b4634003c5f3f888ea4867457bb87d1fa3
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-11 13:40:40 -0300

    Exercicio Lista 2 - Converter

--- aulas_Poo/src/br/com/exercicios_lista2_poo/f_Converter.java
+++ aulas_Poo/src/br/com/exercicios_lista2_poo/f_Converter.java
@@ -2,36 +2,53 @@ package br.com.exercicios_lista2_poo;
 
 import java.util.Scanner;
 import java.util.logging.Logger;
-
 import br.com.aula_poo.utils.Util;
 
 public class f_Converter {
 	private static Logger logger = Logger.getLogger(Util.class.getName());
-	
-    public static void main(String[] args) throws InterruptedException {
-        Scanner scanner = new Scanner(System.in);
-    
-        logger.info("insira hora e minutos para conversão em segundos no seguinte formato - xx,xx\n");
-        double hora = scanner.nextDouble();
-        double segundo = hora * 3600;
-        String mensagemHora = String.format("Você inseriu %.2f horas.\n", hora);
-        logger.warning("Aguarde\n");
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		// Criando um scanner para ler a entrada do usuário
+		Scanner scanner = new Scanner(System.in);
+
+		logger.info("Insira hora e minutos para conversão em segundos no seguinte formato - xx,xx\n");
+		
+		// Lê a entrada do usuário
+		double hora = scanner.nextDouble();
+		
+		// Calculo
+		double segundo = hora * 3600;
+
+		// Exibe a hora inserida
+		String mensagemHora = String.format("Você inseriu %.2f horas.\n", hora);
+		logger.info(mensagemHora);
+		logger.warning("Aguarde\n");
 		Thread.sleep(3000);
-        logger.info("Para converter horas em segundos, efetuamos o calculo:\n");
-        logger.info("Segundos = Horas x 3600\n");
-        Thread.sleep(3000);
-        logger.warning("Convertendo\n");
-        logger.info(".");
-		Thread.sleep(1000);
+
+		// Mostra o cálculo da conversão
+		logger.info("Para converter horas em segundos, efetuamos o cálculo:\n");
+		logger.info("Segundos = Horas x 3600\n");
+		Thread.sleep(3000);
+
+		// Mensagem de conversão
+		logger.warning("Convertendo\n");
 		logger.info(".");
 		Thread.sleep(1000);
 		logger.info(".");
 		Thread.sleep(1000);
-        String mensagemCalculo = String.format("Segundos = %.2f x 3600", hora);
-        logger.info(mensagemCalculo);
-        String mensagemResultado = String.format("Segundos = %.2f", segundo);
-        logger.info(mensagemResultado + "s");
-       
-        scanner.close();  
-    }
+		logger.info(".");
+		Thread.sleep(1000);
+
+		// Mostra o cálculo em detalhes
+		String mensagemCalculo = String.format("Segundos = %.2f x 3600", hora);
+		logger.info(mensagemCalculo);
+
+		// Exibe o resultado
+		String mensagemResultado = String.format("Segundos = %.2f", segundo);
+		logger.info(mensagemResultado + "s");
+
+		// Fecha o scanner
+		scanner.close();
+	}
 }
