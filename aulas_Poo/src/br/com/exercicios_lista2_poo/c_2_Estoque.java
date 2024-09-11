40179cbec4952c759667708998d99ba7bddc7fc2
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-11 14:28:47 -0300

    Atualização exercicio Estoque

--- aulas_Poo/src/br/com/exercicios_lista2_poo/c_2_Estoque.java
+++ aulas_Poo/src/br/com/exercicios_lista2_poo/c_2_Estoque.java
@@ -1,17 +1,21 @@
 package br.com.exercicios_lista2_poo;
 
 import java.util.Scanner;
+import java.util.logging.Logger;
+
+import br.com.aula_poo.utils.Util;
 
 public class c_2_Estoque {
+	private static Logger logger = Logger.getLogger(Util.class.getName());
 
 	public static void main(String[] args) {
-		String[] estoque = { "Mouse", "Teclado", "Monitor", "SSD", "Placa de vídeo" };
+		String[] estoque = { "Mouse - 10un", "Teclado - 15un", "Monitor - 5un", "SSD - 3un", "Placa de vídeo - 2un" };
 		String[] semEstoque = { "Caixa de som", "Placa de vídeo", "Mousepad", "Cadeira Gamer" };
 		
 		boolean programa = true;
 		
 		while (programa) {
-			System.out.println("Escolha as opções que deseja visualizar:\n" + "(1) Produtos em estoque\n"
+			logger.warning("Escolha as opções que deseja visualizar:\n" + "(1) Produtos em estoque\n"
 					+ "(2) Produtos sem estoque\n(3) Sair");
 			Scanner scanner = new Scanner(System.in);
 			int opcao = scanner.nextInt();
@@ -19,38 +23,40 @@ public class c_2_Estoque {
 			switch (opcao) {
 			case 1:
 				// Produtos em estoque
-				System.out.println("Produtos em estoque:");
+				logger.info("Produtos em estoque:");
 				for (String produto : estoque) {
-					System.out.println(produto);
+					logger.info(produto);
 				}
 				break;
 
 			case 2:
 				// Produtos sem estoque
-				System.out.println("Produtos sem estoque:");
+				logger.info("Produtos sem estoque:");
 				for (String produto : semEstoque) {
-					System.out.println(produto);
+					logger.info(produto);
 				}
 				break;
 
 			case 3:
 				// Confirma se o usuário realmente deseja sair
-				System.out.println("Deseja realmente sair (S/N)?");
+				logger.info("Deseja realmente sair (S/N)?");
 				String escolha = scanner.next();
 
 				// Verifica a escolha do usuário
 				switch (escolha.toUpperCase()) {
 				case "S":
-					System.out.println("Obrigado por utilizar nosso programa :)");
+					logger.info("Obrigado por utilizar nosso programa :)");
 					programa = false; 
 					break;
 
 				case "N":
-					System.out.println("Voltando ao menu...");
+					logger.info("Voltando ao menu...");
 					break;
+					
 				}
+				 scanner.close();
 			}
-			 scanner.close();
 		}
+		
 	}
 }
