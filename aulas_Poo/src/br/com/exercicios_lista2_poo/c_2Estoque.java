85a7638df6e615e02ccf6e8a37eb967543b86759
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-11 14:18:13 -0300

    Exercício lista 2 - Estoque

--- aulas_Poo/src/br/com/exercicios_lista2_poo/c_2Estoque.java
+++ aulas_Poo/src/br/com/exercicios_lista2_poo/c_2Estoque.java
@@ -1,36 +1,56 @@
-package br.com.exercicios_poo;
+package br.com.exercicios_lista2_poo;
 
 import java.util.Scanner;
 
-//(Desafio) O programa "Estoque" irá solicitar que o cliente acesse um menu
-//e escolha entre as opções (Listar Produtos com estoque; Listar Produtos
-//sem estoque; Sair). Para gerar as listas será preciso simular um banco
-//(vetores ou matrizes). Para cada opção, exibe a lista correspondente. Para a
-//opção Sair, perguntar se deseja, realmente, sair e encerra o programa se a
-//resposta for sim.
 public class c_2Estoque {
 
 	public static void main(String[] args) {
 		String[] estoque = { "Mouse", "Teclado", "Monitor", "SSD", "Placa de vídeo" };
 		String[] semEstoque = { "Caixa de som", "Placa de vídeo", "Mousepad", "Cadeira Gamer" };
+		
+		boolean programa = true;
+		
+		while (programa) {
+			System.out.println("Escolha as opções que deseja visualizar:\n" + "(1) Produtos em estoque\n"
+					+ "(2) Produtos sem estoque\n(3) Sair");
+			Scanner scanner = new Scanner(System.in);
+			int opcao = scanner.nextInt();
 
-		System.out.println("Escolha as opções que deseja visualizar:\n" + "(1) Produtos em estoque\n"
-				+ "(2) Produtos sem estoque\n");
-		Scanner scanner = new Scanner(System.in);
-		int opcao = scanner.nextInt();
+			switch (opcao) {
+			case 1:
+				// Produtos em estoque
+				System.out.println("Produtos em estoque:");
+				for (String produto : estoque) {
+					System.out.println(produto);
+				}
+				break;
 
-		if (opcao == 1) {
-			for (String estoques : estoque) {
-				System.out.println(estoques + " ");
+			case 2:
+				// Produtos sem estoque
+				System.out.println("Produtos sem estoque:");
+				for (String produto : semEstoque) {
+					System.out.println(produto);
+				}
+				break;
+
+			case 3:
+				// Confirma se o usuário realmente deseja sair
+				System.out.println("Deseja realmente sair (S/N)?");
+				String escolha = scanner.next();
+
+				// Verifica a escolha do usuário
+				switch (escolha.toUpperCase()) {
+				case "S":
+					System.out.println("Obrigado por utilizar nosso programa :)");
+					programa = false; 
+					break;
+
+				case "N":
+					System.out.println("Voltando ao menu...");
+					break;
+				}
 			}
-		} else if (opcao == 2) {
-			for (String estoques2 : semEstoque) {
-				System.out.println(estoques2 + " ");
-			}
-		} else {
-			System.out.println("Opção inválida");
-
+			 scanner.close();
 		}
-
 	}
 }
