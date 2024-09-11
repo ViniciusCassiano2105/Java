b3bb3c348b4be0a018cb2666f3a510a8ce9a5fc5
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-11 16:42:39 -0300

    Exercício lista 1 - Atena

--- aulas_Poo/src/br/com/exercicios_lista1_poo/d_atena.java
+++ aulas_Poo/src/br/com/exercicios_lista1_poo/d_atena.java
@@ -1,20 +1,38 @@
-package br.com.exercicios_poo;
+package br.com.exercicios_lista1_poo;
 
 import java.util.Scanner;
+import java.util.logging.Logger;
+
+import br.com.aula_poo.utils.Util;
 
 public class d_atena {
 
+	private static Logger logger = Logger.getLogger(Util.class.getName());
+
 	public static void main(String[] args) {
+		// Declaração de variáveis
 		int cels;
-		int fahr;
-		
-		System.out.println("Digite uma temperatura em Celsius para converter em Fahrenheit:");
+
+		// Mensagem solicitando a entrada de uma temperatura em Celsius
+		logger.info("Digite uma temperatura em Celsius para converter em Fahrenheit:");
+
+		// Criando um scanner para ler a entrada do usuário
 		Scanner scanner = new Scanner(System.in);
+
+		// Lê a temperatura em Celsius fornecida pelo usuário
 		cels = scanner.nextInt();
-		fahr = (int) (cels * 1.8 + 32);
-		System.out.printf("A temperatura em Fahrenheit é %dº", fahr);
-				
 
+		// Converte a temperatura de Celsius para Fahrenheit usando a fórmula (Celsius *
+		// 1.8 + 32)
+		int fahr = (int) (cels * 1.8 + 32);
+
+		// Formata a mensagem para exibir a temperatura convertida
+		String mensagemTemperatura = String.format("A temperatura em Fahrenheit é %dº", fahr);
+
+		// Exibe a mensagem
+		logger.info(mensagemTemperatura);
+
+		// Fecha o scanner
+		scanner.close();
 	}
-
 }
