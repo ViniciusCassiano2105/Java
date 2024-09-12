53047b5ee0c1d50eb18e6a7106be8d3db046d6b7
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-12 07:13:24 -0300

    Correção lista 2 - Estoque

--- aulas_Poo/src/br/com/exercicios_lista2_poo/c_Estoque.java
+++ aulas_Poo/src/br/com/exercicios_lista2_poo/c_Estoque.java
@@ -8,21 +8,23 @@ import br.com.aula_poo.utils.Util;
 public class c_Estoque {
 	private static Logger logger = Logger.getLogger(Util.class.getName());
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		// Produtos em estoque e fora de estoque
 		String[] estoque = { "Mouse - 10un", "Teclado - 15un", "Monitor - 5un", "SSD - 3un", "Placa de vídeo - 2un" };
 		String[] semEstoque = { "Caixa de som", "Placa de vídeo", "Mousepad", "Cadeira Gamer" };
 
 		boolean programa = true;
 
+		// Criando o scanner fora do laço
+		Scanner scanner = new Scanner(System.in);
+
 		while (programa) {
 			logger.warning("Escolha as opções que deseja visualizar:\n" + "(1) Produtos em estoque\n"
 					+ "(2) Produtos sem estoque\n(3) Sair");
-			
-			// Criando um scanner para ler a entrada do usuário
-			Scanner scanner = new Scanner(System.in);
-			int opcao = scanner.nextInt();
 
-			//Escolhas do usuário
+			int opcao = scanner.nextInt(); // Leitura da escolha do usuário
+
+			// Escolhas do usuário
 			switch (opcao) {
 			case 1:
 				// Produtos em estoque
@@ -43,23 +45,32 @@ public class c_Estoque {
 			case 3:
 				// Confirma se o usuário realmente deseja sair
 				logger.info("Deseja realmente sair (S/N)?");
-				String escolha = scanner.next();
+				String escolha = scanner.next(); // Leitura da confirmação de saída
 
 				// Verifica a escolha do usuário
 				switch (escolha.toUpperCase()) {
 				case "S":
 					logger.info("Obrigado por utilizar nosso programa :)");
-					programa = false;
+					programa = false; // Sai do laço
 					break;
 
 				case "N":
 					logger.info("Voltando ao menu...");
+					Thread.sleep(4000);
 					break;
 
+				default:
+					logger.info("Escolha inválida, voltando ao menu...");
+					break;
 				}
-				scanner.close();
+				break;
+
+			default:
+				logger.info("Opção inválida! Tente novamente.");
 			}
 		}
 
+		// Fecha o scanner
+		scanner.close();
 	}
 }
