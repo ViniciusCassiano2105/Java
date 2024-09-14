dd7d45e14c14b7ef034c230f8bfda43570eec8f7
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-14 12:07:26 -0300

    Projeto individual Cinema/Filme - POO

---
+++ projeto_Individual_Poo/src/projeto_Individual_Poo/Cinema.java
@@ -0,0 +1,5 @@
+package projeto_Individual_Poo;
+
+public class Cinema {
+
+}
---
+++ projeto_Individual_Poo/src/projeto_Individual_Poo/Menu.java
@@ -0,0 +1,9 @@
+package projeto_Individual_Poo;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		System.out.println();
+	}
+
+}
