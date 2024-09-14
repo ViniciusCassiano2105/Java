df7d297bebee686398d3e7171c7a8d6badda91dd
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-14 16:53:40 -0300

    Mensagens iniciais no menu

--- projeto_Individual_Poo/src/br/com/cinema_filme_menu_poo/Menu.java
+++ projeto_Individual_Poo/src/br/com/cinema_filme_menu_poo/Menu.java
@@ -1,9 +1,29 @@
 package br.com.cinema_filme_menu_poo;
 
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+import br.com.aula_poo.utils.Util;
+
 public class Menu {
+	private static Logger logger = Logger.getLogger(Util.class.getName());
 
 	public static void main(String[] args) {
-		System.out.println();
-	}
 
+		boolean escolha = true;
+		
+		Scanner scanner = new Scanner(System.in);
+
+		logger.info(Mensagem.BEM_VINDO);
+		logger.info(Mensagem.LOGO);
+		logger.info(Mensagem.PERGUNTA_NOME);
+		String nome = scanner.next();
+		while (escolha) {
+			logger.info(Mensagem.ESCOLHA);
+			logger.info(Mensagem.EXBIR_RELATORIO);
+			logger.info(Mensagem.GERAR_RELATORIO);
+			logger.info(Mensagem.SAIR);
+
+		}
+	}
 }
