a3838b9aee085158b213fe8e54e73e16f7020c42
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-14 16:54:13 -0300

    Classe mensagem (mensagens do programa)

---
+++ projeto_Individual_Poo/src/br/com/cinema_filme_menu_poo/Mensagem.java
@@ -0,0 +1,15 @@
+package br.com.cinema_filme_menu_poo;
+
+public class Mensagem {
+
+	// Mensagens Iniciais
+	public static final String BEM_VINDO = "\n---------------OLA SEJA BEM VINDO(A) A NOSSA APLICAÇÃO---------------\n\n";
+	public static final String LOGO = "\n\t\t\t    IMPERIAL\n\n\t\t\t      FLIX\n";
+	public static final String PERGUNTA_NOME = "Digite como gostaria de ser chamado(a):";
+
+	// Mensagens de escolhas
+	public static final String ESCOLHA = "Escolha uma opção:";
+	public static final String EXBIR_RELATORIO = "(1) Exibir relatório na tela";
+	public static final String GERAR_RELATORIO = "(2) Gerar relatório impresso";
+	public static final String SAIR = "(3) Sair";
+}
