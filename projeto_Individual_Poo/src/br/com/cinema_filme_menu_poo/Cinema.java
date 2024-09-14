99f988cf152f151bf2f14e4f86fb0d699d27bcc9
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-14 14:58:40 -0300

    Get/Set classe - Cinema

--- projeto_Individual_Poo/src/br/com/cinema_filme_menu_poo/Cinema.java
+++ projeto_Individual_Poo/src/br/com/cinema_filme_menu_poo/Cinema.java
@@ -5,6 +5,36 @@ public class Cinema {
 	int sala;
 	double valor;
 	String nome;
+	public int getIdcinema() {
+		return idcinema;
+	}
+	public void setIdcinema(int idcinema) {
+		this.idcinema = idcinema;
+	}
+	public int getSala() {
+		return sala;
+	}
+	public void setSala(int sala) {
+		this.sala = sala;
+	}
+	public double getValor() {
+		return valor;
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getHorario() {
+		return horario;
+	}
+	public void setHorario(String horario) {
+		this.horario = horario;
+	}
 	String horario;
 	
 	
