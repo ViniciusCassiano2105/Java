5d6afb03e3d443464043be0b66458307991a6363
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-18 11:32:18 -0300

    adição de mensagens

--- projeto_Individual_Poo/src/br/com/cinema_filme_menu_poo/Mensagem.java
+++ projeto_Individual_Poo/src/br/com/cinema_filme_menu_poo/Mensagem.java
@@ -3,25 +3,49 @@ package br.com.cinema_filme_menu_poo;
 public class Mensagem {
 
 	// Mensagens Iniciais
-	public static final String BEM_VINDO = "\n---------------OLÁ SEJA BEM VINDO(A) A NOSSA APLICAÇÃO---------------\n\n";
+	public static final String BEM_VINDO = "\n--------------- SEJA BEM VINDO(A) A MAIOR REDE DE CINEMAS DO BRASIL ---------------\n\n";
 	public static final String LOGO = "\n\t\t\t    IMPERIAL\n\n\t\t\t      FLIX\n";
 	public static final String PERGUNTA_NOME = "Digite como gostaria de ser chamado(a):";
 
 	// Mensagens de escolhas
-	public static final String ESCOLHA = "Olá %s escolha uma opção:";
-	public static final String EXIBIR_RELATORIO = "(1) Exibir relatório na tela";
-	public static final String GERAR_RELATORIO = "(2) Gerar relatório impresso";
+	public static final String OLA = "Olá %s escolha uma opção:";
+	public static final String ESCOLHAOPCAOEXIBIR = "Escolha uma opção para exibir:";
+	public static final String ESCOLHAEXIBIR = "(1) Exibir Informações";
+	public static final String ESCOLHAGERAR = "(2) Gerar Ticket Impresso";
+	public static final String EXIBIR_RELATORIO_FILMES = "(3) Filmes em cartaz";
+	public static final String EXIBIR_RELATORIO_CINEMA = "(4) rede de Cinemas";
+	public static final String ESCOLHAOPCAOGERAR = "Escolha uma opção para gerar:";
+	public static final String GERAR_RELATORIO_FILMES = "(3) Ticket de Filmes";
+	public static final String GERAR_RELATORIO_CINEMA = "(4) Ticket de Cinemas";
+	public static final String VOLTARMENU = "(5) Voltar ao menu principal";
+	public static final String DESEJASAIR = "Deseja sair do sistema? (S/N)";
 	public static final String SAIR = "(3) Sair";
-	public static final String AGUARDE = "Aguarde :)";
+	public static final String SAIRSUB = "(5) Sair";
 	
+
+	// Mensagens de espera
+	public static final String AGUARDE = "Aguarde :)";
+	public static final String EXIBINDO_RELATORIO_FILMES = "Exibindo Filmes em cartaz...";
+	public static final String EXIBINDO_RELATORIO_CINEMA = "Exibindo nossa rede de Cinemas...";
+	public static final String GERANDO_RELATORIO_FILMES = "Gerando Ticket Filmes em cartaz...";
+	public static final String GERANDO_RELATORIO_CINEMA = "Gerando Ticket da nossa rede de Cinemas...";
+	public static final String VOLTANDOMENU = "Voltando ao menu principal...";
+	
+
 	// Mensagens opção
 	public static final String OPCAO_EXIBIR_RELATORIO = "Exibindo relatório";
 	public static final String OPCAO_GERAR_RELATORIO = "Imprimindo relatório";
 	public static final String OPCAO_PERGUNTA_SAIR_SISTEMA = "Deseja realmente sair:\n\t\t(S/N) ";
 	public static final String OPCAO_SIM_OBRIGADO_UTILIZAR_SISTEMA = "Obrigado por utilizar nossa aplicação, até breve! :P";
-	public static final String OPCAO_NAO_VOLTANDO_SISTEMA = "Voltando para o MENU";
+
+	
+	// Mensagem de loop
+	public static final String OPCAO_VOLTANDO_MENU = "Voltando para o MENU";
+
+	//	Mensagens de saudação
+	public static final String OBRIGADOVISITA = "Obrigado por nos visitar, volte sempre!";
+
+	// Mensagem de erro
 	public static final String OPCAO_INVALIDA = "Opção invalida, erro 404...";
-	
-	
-	
+
 }
